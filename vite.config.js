cd651c65e5ed44b3ffcab6b4821e31659056f0d6
KatanovaLesya <katanovalesya@gmail.com>
2024-12-11 16:11:05 +0200

    +

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/goit-neo-react-hw-module3/', // Встановлено коректно base
   plugins: [react()],
   resolve: {
     alias: {
-      '@:': '/src',
+      '@': '/src', // Виправлено синтаксис
     }
   }
 })
